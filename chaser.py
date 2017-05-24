753f5af033989d179832f611f379f7e2a56fe993
DotBowder <D.Bowder@gmail.com>
2017-05-24 07:05:37 -0700

    Update chaser.py
    Complete Re-Write.
    Code not functioning fully at this time.

--- chaser.py
+++ chaser.py
@@ -3,256 +3,143 @@ import sys
 import pygame
 import random
 from pygame.locals import *
-from math import pi
 
-
-# Static Vars
+#Initialize Variables
 WHITE = 255,255,255
 GREY = 128,128,128
 BLACK = 0,0,0
-up, right, down, left = 0, 1, 2, 3
 
-# Function for generating a new reward sprite
-def rewardGen():
-	newX = random.randint(0, windowSize[0]-rewardSize[0])
-	newY = random.randint(0, windowSize[1]-rewardSize[1])
-	return (newX,newY)
+canvasSize = (100,100)
 
-
-
-############
-### Init ###
-############
-#
-# Init functions to get our declarations and setup out of the way.
-#
-
-def InitCreateWindow(requestedSize):
-	global windowSize
-	global window
+################
+# Canvas Class #
+################ 
+class Canvas():
+	X = int
+	y = int
 	
-	windowSize = requestedSize
-	window = pygame.display.set_mode(windowSize)
-	
-	# We need a window to use for pygame
-
-def InitDrawPlayer(playersize):
-	global playerScore
-	global playerMov
-	global playerSpeed
-	global playerSize
-	global playerPos
-	global playerRect
-	global playerTotalMoves
-	
-	playerTotalMoves = 0
-	playerScore = 0
-	playerMov = random.randint(0,3)
-	playerSpeed = 1
-	playerSize = [playersize,playersize]
-	playerPos = [windowSize[0]/2,windowSize[1]/2]
-	playerRect = pygame.Rect(playerPos[0],playerPos[1],playerSize[0],playerSize[1])
-	
-	# Declare player variables
-
-def InitDrawReward():
-	global rewardCount
-	global rewardValue
-	global rewardSize
-	global rewardPos
-	global rewardRect
+	def __init__(self,size):
+		global canvas
 		
-	rewardCount = 0
-	rewardValue = 10
-	rewardSize = playerSize
-	rewardPos = rewardGen()
-	rewardRect = pygame.Rect(rewardPos[0],rewardPos[1],rewardSize[0],rewardSize[1])
-	
-	# Declare reward variables
+		self.W = size[0]
+		self.H = size[1]
+		canvas = pygame.display.set_mode(size)
 
-def InitPlayerDist():
-	global playerDist
-	
-	playerDist = LivePlayerDist()
-	
-	# Declare player's initial distance from reward sprite
-
-def InitProcessInputs():
-	global reqMov
-	
-	reqMov = playerMov
-	
-	# Declare the requested move variable so that it can be used later
-
-
-
-############
-### Live ###
-############
+################
+# Player Class #
+################
+# The Player Class has position variables for easily determining the player location.
 #
-# Live functions will be stepped through, to generate the game frame by frame.
-#
+# Variables:
+# W = width, H = height, X = Upper Left Corner X Val, Y = Upper Left Corner Y Val
+# Center = Pixel (X,Y) in center of player sprite.
+# Rect = Rectangle defining player sprite draw bounds on canvas.
+# Speed = Distance to move sprite accross canvas per frame. Set to 1/100 of the canvas size.
+# 
+# Functions:
+# UpdateRect() updates the player sprite rectangle. This does NOT draw the canvas sprite.
+# Move() updates the player sprite location. 
+# Draw() draws the player sprite location to the canvas.
+# 
+################
 
-def LiveDrawPlayer():
-	global playerRect
-	playerRect = pygame.Rect(playerPos[0],playerPos[1],playerSize[0],playerSize[1])
-	player = pygame.draw.rect(window, WHITE, playerRect, 0)
+class Player():
+	W = canvasSize[0]/10 				#Default 1/10 of screen size
+	H = canvasSize[1]/10 				#Default 1/10 of screen size
+	X = (canvasSize[0]/2) - (W/2) 		#Default to center of screen
+	Y = (canvasSize[1]/2) - (H/2) 		#Default to center of screen
+	Center = (X - W/2),(Y - H/2)
+	Rect = (int,int,int,int)			#Placeholder for later definition
+	Speed = canvasSize[0]/100
 	
-	# Use the player variables to draw a player Rectangle
-
-
-def LiveDrawReward():
-	global rewardRect
-	rewardRect = pygame.Rect(rewardPos[0],rewardPos[1],rewardSize[0],rewardSize[1])
-	reward = pygame.draw.rect(window, GREY, rewardRect, 0)
+	def UpdateRect(self):
+		self.Rect = pygame.Rect(self.X,self.Y,self.W,self.H)
 	
-	# Use the reward variables to draw a reward rectangle
-
-def LiveProcessInputs():
-	global playerMov
-	global reqMov
-	pygame.event.get()					
-	keys = pygame.key.get_pressed()
-	if keys[K_UP]:
-		reqMov = up
-	elif keys[K_RIGHT]:
-		reqMov = right
-	elif keys[K_DOWN]:
-		reqMov = down
-	elif keys[K_LEFT]:
-		reqMov = left
-	else:
-		reqMov = playerMov
-	playerMov = reqMov
-	
-	# Pull keypress data and check if up,right,down,or left is pressed. If so, check if the direction is eligible and change playerMov to the new direction.
-def LiveNNAction(actions):
-	global playerMov
-	global reqMov
-	
-	if actions[0] and playerMov != down:
-		reqMov = up
-	elif actions[1] and playerMov != left:
-		reqMov = right
-	elif kactions[2] and playerMov != up:
-		reqMov = down
-	elif actions[3] and playerMov != right:
-		reqMov = left
-	else:
-		reqMov = playerMov
-	playerMov = reqMov
-	
-	# Pull keypress data and check if up,right,down,or left is pressed. If so, check if the direction is eligible and change playerMov to the new direction.
-
-
-def LiveMovePlayer():
-	global playerMov
-	global playerPos
-	global playerSpeed
-	global playerTotalMoves
-	
-	if playerMov == up and (playerPos[1] - playerSpeed) > 0:
-		playerPos[1] = playerPos[1] - playerSpeed
-	elif playerMov == right and (playerPos[0] + playerSpeed + playerSize[0]) < windowSize[0]:
-		playerPos[0] = playerPos[0] + playerSpeed
-	elif playerMov == down and (playerPos[1] + playerSpeed + playerSize[1]) < windowSize[1]:
-		playerPos[1] = playerPos[1] + playerSpeed
-	elif playerMov == left and (playerPos[0] - playerSpeed) > 0:
-		playerPos[0] = playerPos[0] - playerSpeed
-	else:
-		pass
-	playerTotalMoves = playerTotalMoves + 1
-	
-	# Update player position based on playerMov variable.
-	# playerMov reports the direction of the player.
-	# 0 is up, 1 is right, 2 is down, 3 is left
-	# playerSpeed is the variable that tells us how many pixels we want to jump. 
-	# This can be useful for speeding up the game, however, it would be unwize to set the playerSpeed
-	# to a bigger number than playerSize + rewardSize as that would create gaps where the player
-	# could hop right over the reward.
-
-def LiveAssessReward():
-	global rewardPos
-	global playerScore
-	global playerRect
-	global rewardRect
-	global playerDist
-	global playerTotalMoves
-	global playerExcessMoves
-	
-	collisionStatus = pygame.Rect.colliderect(playerRect, rewardRect)
-	if collisionStatus == True:
-		playerScore = playerScore + rewardValue
-		playerExcessMoves = playerTotalMoves - playerDist
-		if playerExcessMoves < 0:
-			playerExcessMoves = 0
-		print 'Player Score:',playerScore,'\tPrev PlayerDist:',playerDist,'\tExcess Moves:',playerExcessMoves,'\tTotal Moves:',playerTotalMoves
-		rewardPos = rewardGen()
-		playerDist = LivePlayerDist()
-		playerTotalMoves = 0
-	else:
-		pass
-	
-	# LiveAssessReward determines if the playerRect and the rewardRect are colliding.
-	# If they collide, then we need to + the playerScore, calculate the excess moves used by the player,
-	# generate a new reward sprite by randomly generating new X,Y coords, and reset the player movecount.
-
-def LiveExportWindow():
-	image_data = pygame.surfarray.array3d(pygame.display.get_surface())
-	return image_data
-	
-	# This is used to return a 3d array of the pixels on the screen.
-	# This will be used as input for a Machine Learning neural network.
-
-def LivePlayerDist():
-	global playerDist
-	
-	dist = abs(playerPos[0]-rewardPos[0]) + abs(playerPos[1]-rewardPos[1])
-	dist = dist / playerSpeed
-	return dist
-	
-	# This function calculates the minimum physical moves that the player could take to reach the reward sprite. (Doesn't account for sprite size and is tied to upper left corner coords of each sprite.)
-
-
-###
-# Class
-###
-#
-# The class wraps up all of our functions into one nice little container allowing this to be called in another python script.
-# More passthrough variables will be added at a later time.
-#
-
-class Game:
-	def __init__(self,playersize,mapsize):
-		# Run Init
-		InitCreateWindow(mapsize)
-		InitDrawPlayer(playersize)
-		InitDrawReward()
-		InitPlayerDist()
-		InitProcessInputs()
+	def Move(self,direction):
+		if direction == 'up':
+			if self.Y > 0:
+				self.Y = self.Y - self.Speed
+		if direction == 'down':
+			if self.Y < canvasSize[1] - self.H:
+				self.Y = self.Y + self.Speed
+		if direction == 'left':
+			if self.X > 0:
+				self.X = self.X - self.Speed
+		if direction == 'right':
+			if self.X < canvasSize[0] - self.W:
+				self.X = self.X + self.Speed
+		self.Center = (self.X - self.W/2),(self.Y - self.H/2)
 		
 	def Draw(self):
-		# Run Live
-		window.fill(BLACK)			# Wipe Screen
-		LiveDrawPlayer()			# Draw Player
-		LiveDrawReward()			# Draw Reward
-		LivePlayerDist()			# Get Player Distance from Reward
-		
+		self.UpdateRect()
+		pygame.draw.rect(canvas, WHITE, self.Rect, 0)
 
-	def Process(self,delay, action = (0,0,0,0)):
-		LiveProcessInputs()		# Did the user press a key? If so, update playerMov Direction (Will eventually be replaced with an action variable passed through by neural network at a later time.)
-		#LiveNNAction()				# Process NN Action List
-		LiveMovePlayer()			# Move Player toward playerMov Direction
-		LiveAssessReward()			# Did the player collide with the Reward?
-		pygame.display.update() 	# Refresh the display.
-		pygame.time.delay(delay)		# Wait X ms 
 
-	def GetFrame(self):
-		return LiveExportWindow()			# Returns a 3d array of pixel data.
-		
-	def GetScore(self):
-		return playerScore
-		
-	def GetPlayerDist(self):
-		return LivePlayerDist()
+################
+# Reward Class #
+################ 
 
+class Reward():
+	W = canvasSize[0]/10
+	H = canvasSize[1]/10
+	X = random.randint(0,canvasSize[0]-W)
+	Y = random.randint(0,canvasSize[1]-H)
+	Center = (X - W/2),(Y - H/2)
+	Rect = (int,int,int,int)
+	#Speed = canvasSize[0]/100 #May later be used to move rewards on the canvas.
+	
+	def UpdateRect(self):
+		self.Rect = pygame.Rect(self.X,self.Y,self.W,self.H)
+	
+	def Draw(self):
+		self.UpdateRect()
+		pygame.draw.rect(canvas, BLACK, self.Rect, 0)
+
+
+
+##################
+# Keyboard Class #
+##################
+
+class Keyboard():	
+	def Update(self):
+		pygame.event.get()
+		self.keys = pygame.key.get_pressed()
+		if self.keys[K_UP]:
+			return 'up'
+		elif self.keys[K_RIGHT]:
+			return 'right'
+		elif self.keys[K_DOWN]:
+			return 'down'
+		elif self.keys[K_LEFT]:
+			return 'left'
+		else:
+			return 4
+
+
+
+def UpdateScreen():
+	pygame.display.update() 
+	pygame.time.delay(16)
+
+
+
+screen = Canvas(canvasSize)
+canvas.fill(GREY)
+keyboard = Keyboard()
+
+player = Player()
+reward = Reward()
+
+
+player.Draw()
+reward.Draw()
+
+for iteration in range(2000):
+	canvas.fill(GREY)	
+	player.Move(keyboard.Update())
+	player.Draw()
+	reward.Draw()
+	UpdateScreen()
+
+#pygame.time.delay(2000)
